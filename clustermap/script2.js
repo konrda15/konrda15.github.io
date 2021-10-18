4e3906efcdaea624c55054baad8b1aee47b2e5f1
Daniel Konrath <konrath@student.tugraz.at>
2021-10-18 18:37:14 +0200

    add help

--- clustermap/script2.js
+++ clustermap/script2.js
@@ -469,6 +469,34 @@ function drawClusterTable(clusterStats, clusterStats17) {
   clusterTableDiv.innerHTML = content;
 }
 
+//modal https://www.w3schools.com/howto/howto_css_modals.asp
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.querySelector(".info-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
 //helper functions
 function ratioColor(colorrgb, ratio, maxRatio) {
   let relativeRatio = 1 - ratio / maxRatio;
